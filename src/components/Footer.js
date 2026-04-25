f018f861048473a7424745eeb36984383331e36c
Abhishek Bidve <161975106+AbhiBidve7241@users.noreply.github.com>
2026-04-26 00:34:13 +0530

    Fix footer links to use valid URLs
    Updated social media and footer links to point to a valid URL.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,13 +15,13 @@ const Footer = () => {
           <div className="social-links">
             <h4>Follow Us</h4>
             <div className="social-icons">
-              <a href="#" aria-label="Facebook">
+              <a href="https://404notfound.com/" aria-label="Facebook">
                 <i className="fa-brands fa-facebook-f"></i>
               </a>
-              <a href="#" aria-label="Instagram">
+              <a href="https://404notfound.com/" aria-label="Instagram">
                 <i className="fa-brands fa-instagram"></i>
               </a>
-              <a href="#" aria-label="Twitter">
+              <a href="https://404notfound.com/" aria-label="Twitter">
                 <i className="fa-brands fa-twitter"></i>
               </a>
             </div>
@@ -32,9 +32,9 @@ const Footer = () => {
           <h4>Information</h4>
           <ul>
             <li><Link to="/about">About Us</Link></li>
-            <li><a href="#">Courier Partner</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms and Conditions</a></li>
+            <li><a href="https://404notfound.com/">Courier Partner</a></li>
+            <li><a href="https://404notfound.com/">Privacy Policy</a></li>
+            <li><a href="https://404notfound.com/">Terms and Conditions</a></li>
             <li><Link to="/contact">Contact Us</Link></li>
           </ul>
         </div>
@@ -42,21 +42,21 @@ const Footer = () => {
         <div className="footer-section">
           <h4>My Account</h4>
           <ul>
-            <li><a href="#">Sign In</a></li>
+            <li><a href=" ">Sign In</a></li>
             <li><Link to="/cart">View Cart</Link></li>
-            <li><a href="#">Wishlists</a></li>
-            <li><a href="#">Track Order</a></li>
-            <li><a href="#">Help</a></li>
+            <li><a href=" ">Wishlists</a></li>
+            <li><a href=" ">Track Order</a></li>
+            <li><a href=" ">Help</a></li>
           </ul>
         </div>
 
         <div className="footer-section">
           <h4>Install App</h4>
           <div className="app-links">
-            <a href="#">
+            <a href="https://404notfound.com/">
               <img src="/Assets/Footer/play.jpg" alt="Google Play" />
             </a>
-            <a href="#">
+            <a href="https://404notfound.com/">
               <img src="/Assets/Footer/app.jpg" alt="App Store" />
             </a>
           </div>
